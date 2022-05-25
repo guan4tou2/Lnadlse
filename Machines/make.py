9395306a36274810998c3811f302e571b864c718
guantou <mole30608@gmail.com>
2022-05-25 12:34:23 +0800

    Update make.py

--- Machines/make.py
+++ Machines/make.py
@@ -5,14 +5,14 @@ import re
 
 path=os.path.dirname(os.path.realpath(__file__))
 
-targeter_list = "、".join(list(os.listdir(os.path.join(path, "Targeter")))).replace('、packetbeat','')
+targeted_list = "、".join(list(os.listdir(os.path.join(path, "Targeted")))).replace('、packetbeat','')
 attacker_list = "、".join(list(os.listdir(os.path.join(path, "Attacker"))))
 # attacks_list="、".join(list(map(removefile,os.listdir(os.path.join(os.getcwd(),"Attacks")))))
 # beat_list="、".join(list(map(removefile,os.listdir(os.path.join(os.getcwd(),"Beat")))))
 
 parser = argparse.ArgumentParser(prog="make", description="建立攻防環境模組安裝文件")
-parser.add_argument("-T", "--Targeter", default="httpd",
-                    type=str, help=f"請選擇靶機image名稱，有[{targeter_list}]")
+parser.add_argument("-T", "--Targeted", default="httpd",
+                    type=str, help=f"請選擇靶機image名稱，有[{targeted_list}]")
 parser.add_argument("-A", "--Attacker", default="kali",
                     type=str, help=f"請選擇攻擊機image名稱，有[{attacker_list}]")
 # parser.add_argument("-a", "--Attack",default="Dos", type=str, help=f"請選擇攻擊方式，有[{attacks_list}]")
@@ -20,8 +20,8 @@ parser.add_argument("-A", "--Attacker", default="kali",
 args = parser.parse_args()
 
 try:
-    if args.Targeter not in targeter_list.split('、'):
-        raise Exception(f"Targeter '{args.Targeter}' 不存在，請重新選擇{targeter_list}")
+    if args.Targeted not in targeted_list.split('、'):
+        raise Exception(f"Targeted '{args.Targeted}' 不存在，請重新選擇{targeted_list}")
     if args.Attacker not in attacker_list.split('、'):
         raise Exception(f"Attacker '{args.Attacker}' 不存在，請重新選擇{attacker_list}")
     # if args.Attack not in attacks_list.split('、'):print(f"Attack '{args.Attack}' 不存在，請重新選擇{attacks_list}")
@@ -33,7 +33,7 @@ try:
         machines = []
         for _ in result:
             machines.append(_.groups()[0])
-        yml = re.sub(machines[0], args.Targeter, yml)
+        yml = re.sub(machines[0], args.Targeted, yml)
         yml = re.sub(machines[1], args.Attacker, yml)
     with open(os.path.join(path, 'makefile'), 'r') as f:
         makefile = f.read()
@@ -41,7 +41,7 @@ try:
         machines = []
         for _ in result:
             machines.append(_.groups()[0])
-        makefile = re.sub(machines[0], args.Targeter, makefile)
+        makefile = re.sub(machines[0], args.Targeted, makefile)
         makefile = re.sub(machines[1], args.Attacker, makefile)
         makefile = re.sub(machines[2], args.Attacker, makefile)
         makefile = re.sub(machines[3], args.Attacker, makefile)
@@ -50,6 +50,6 @@ try:
         f.write(yml)
     with open(os.path.join(path, 'makefile'), 'w') as f:
         f.write(makefile)
-    print(f"攻防環境模組已設置完成，攻擊機: {args.Targeter} ，靶機: {args.Attacker} ")
+    print(f"攻防環境模組已設置完成，攻擊機: {args.Targeted} ，靶機: {args.Attacker} ")
 except Exception as msg:
     print("攻防環境模組設置錯誤，錯誤原因:",msg)
